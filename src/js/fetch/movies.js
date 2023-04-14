1e91198575ca33372d8ae6861bc113876f77f8e8
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-04-14 17:48:58 +0200

    Create movie.js - fetch
    Fetch for popular and movie by title

---
+++ src/js/fetch/movies.js
@@ -0,0 +1,32 @@
+
+
+// ------ nessesary for work ------ 
+const apiKey = "?api_key=11f568ee70218bec08ad7368f7bb3250";
+const apiUrl = "https://api.themoviedb.org/3/search/movie";
+const popularUrl = "https://api.themoviedb.org/3/movie/popular"
+let page = 1;
+
+
+//  1.    ------ Function - fetch - Popular movies ------ 
+const checkPopular = async (page = 1) => {
+    try{
+      const response = await fetch(popularUrl+apiKey+'&page='+page)
+      const data = await response.json();
+      console.log(data)
+// TO DO function here!
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+//  2.    ------ function fetch - Movie checker - by title ------ 
+// movieTitle is a .value from header input
+const checkMovie = async (movieTitle) => {
+    try {
+      const response = await fetch(apiUrl+apiKey+'&query='+ movieTitle+'&page='+ page);
+      const data = await response.json();
+      console.log(data)
+//TO DO function here!
+    } catch (error) {
+      console.error(error);
+    }}
