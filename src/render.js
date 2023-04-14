f99ca1fffa196bb309215ee25805c64f3e2c1930
MaggieWesolowska <maggiewes07@gmail.com>
2023-04-14 15:48:53 +0200

    added render function edited markup

--- src/render.js
+++ src/render.js
@@ -1,17 +1,40 @@
+const API_KEY = 'b118f38ec77100db6763b4cc83604589';
+const gallery = document.querySelector('.gallery');
+
+const fetchGenres = async () => {
+  const genreArray =
+    `https://api.themoviedb.org/3/genre/movie/list` + `?api_key=${API_KEY}`;
+  try {
+    const response = await fetch(genreArray);
+    const genres = await response.json();
+    return genres;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchGenres();
+
+// Ustalic genres:
+// poiterowac po main genre array z serwera
+// sprawdzic czy id ten sam co w danym movie
+//let genreIds = "";
+//genreIds+= genres z array
+
 export const renderMovies = movies => {
   const markupMovies = movies
     .map(movie => {
       return `
-    <img src="${movie.url}" alt="${movie.id}" loading="lazy" />
+    <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" height=574 alt="${movie.original_title}" data-movie-id="${movie.id}"/>
     <div class="movie-card">
       <p class="movie-title">
-        <b>${movie.title}</b> 
+        <b>${movie.original_title}</b>
       </p>
       <p class="movie-genre">
-        <b>${movie.genres}</b> 
+        <b>${movie.genre_ids}</b>
       </p>
       <p class="movie-year">
-        <b>${movie.year}</b> 
+        <b>${movie.release_date}</b>
       </p>
     </div>
   </a>`;
@@ -21,25 +44,21 @@ export const renderMovies = movies => {
   gallery.insertAdjacentHTML('beforeend', markupMovies);
 };
 
-// DOM.movieGallery.addEventListener(
-//     "click",
-//     async (event) => {
-//       const movieId = event.target.dataset.movieId;
-//       const movie = await fetchSingleRocket(movieId);
+const fetchTrendingMovies = async () => {
+  const movieGallery = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`;
+  try {
+    const response = await fetch(movieGallery);
+    const movies = await response.json();
+    return movies;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//       const movieImage = rocket.flickr_images
-//         .map((image) => {
-//           return `<img src="${image}" />`;
-//         })
-//         .join("");
+const startRenderingTrendingMovies = async () => {
+  const fetchedTrendingMovies = await fetchTrendingMovies();
+  console.log(fetchedTrendingMovies);
+  renderMovies(fetchedTrendingMovies.results);
+};
 
-//       rocketDetailsContainer.innerHTML = `
-//       <h2>${rocket.rocket_name}</h2>
-//       <p>Height: ${Object.entries(rocket.height)}</p>
-//       <p>Mass: ${Object.entries(rocket.mass)}</p>
-//       <p>First Flight: ${rocket.first_flight}</p>
-//       <p>${rocket.description}</p>
-//       <div class="rocket-images">${rocketImages}</div>
-//       `;
-//     }
-//   );
+startRenderingTrendingMovies();
