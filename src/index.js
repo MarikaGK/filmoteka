a81f076fd0887e62d6e1ee1315070b667f004c32
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-04-15 19:33:12 +0200

    odkomentowanie funkcji
    Console.logi z bazami dla Maggie

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import {getMovie, getPopular, getGenres} from './js/fetches/movies.js'
 
 //for result for "Rambo" check console and use example from below
-// getMovie('Rambo');
-// getPopular();
-// getGenres();
+getMovie('Rambo');
+getPopular();
+getGenres();
 
 
 import { renderMovies } from './render';
