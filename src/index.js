ab1ff91b919fe1a59d763e3e374d474e1768bd62
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-04-15 22:32:07 +0200

    add search event handler, update fetch functions in movies.js, update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { getMovie, getPopular, getGenres } from './js/fetches/movies.js';
+//*? Powyższy import nie jest jeszcze używany - możliwe, że nie będzie potrzebny w ogóle.
+import { handleSubmit } from './js/search-form-handler.js';
+
+const FORM_DOM = document.querySelector('.header-input__form');
+
+FORM_DOM.addEventListener('submit', handleSubmit);
 
 //for result for "Rambo" check console and use example from below
 getMovie('Rambo');
 getPopular();
 getGenres();
 
-import { renderMovies } from './js/rendering/render.js';
+// import { renderMovies } from './js/rendering/render.js';
