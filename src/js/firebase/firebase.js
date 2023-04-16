4856cf05c0b561a179ff075ce6a0586ce32fcf1a
AlinaBielska <alinkabelka5@gmail.com>
2023-04-16 16:25:12 +0200

    zainstalowany firebase

--- src/js/firebase/firebase.js
+++ src/js/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 // jeszcze nie wiem czy będzie potrzebne
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,5 +23,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 //jeszcze nie wiem czy będzie potrzebne
