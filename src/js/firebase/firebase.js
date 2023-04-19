18679a393abd93ec1d26cbdc661e514416a5e704
AlinaBielska <alinkabelka5@gmail.com>
2023-04-19 15:48:10 +0200

    zaimportowałam auth from firebase i dodałam przykład funkcji do rejestracji i logowania usera

--- src/js/firebase/firebase.js
+++ src/js/firebase/firebase.js
@@ -1,7 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, child, update, onValue } from 'firebase/database';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 // jeszcze nie wiem czy będzie potrzebne
+
+// const firebase = require('firebase');
+// const firebaseui = require('firebaseui');
+// Initialize the FirebaseUI Widget using Firebase.
+// const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 const firebaseConfig = {
   apiKey: "AIzaSyC3WI9OwBz4EKjWjZ6_OIwGrF26sBcAXyE",
@@ -17,9 +23,55 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 // const analytics = getAnalytics(app);
 //jeszcze nie wiem czy będzie potrzebne
 
+//AUTHENTICATION
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+
+
+
+
+
+
+//REALTIME DATABASE 
 
 export function pushToWatched(id) {
   id = 67890966;
@@ -38,7 +90,7 @@ export function pushToQueue(id) {
   console.log(movieId);
   const newPostKey = push(child(ref(db), 'queue')).key;
   const updates = {};
-  updates['/queue/' + newPostKey] = postData;
+  updates['/queue/' + newPostKey] = movieId;
   return update(ref(db), updates);
 }
 
