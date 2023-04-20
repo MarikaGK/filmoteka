a6006c3f4036f4419b08077a7d8005d43d2f9ed9
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-04-20 15:57:06 +0200

    implementacja biblioteki fancybox - wyswietlanie trailera

--- src/js/rendering/render-trailer-link.js
+++ src/js/rendering/render-trailer-link.js
@@ -1,6 +1,13 @@
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
 const trailerLink = document.querySelector('.modal-card__trailer');
 
 export const renderTrailerLink = ytLink => {
-  const markupTrailer = `<a class="modal-card__yt-link link" href="${ytLink}"><div class="yt-logo"></div><p>Watch the trailer</p></a>`;
+  const markupTrailer = `<a class="modal-card__yt-link link" href="${ytLink}" data-fancybox><div class="yt-logo"></div><p>Watch the trailer</p></a>`;
   trailerLink.innerHTML = markupTrailer;
+
+  Fancybox.bind('[data-fancybox]', {
+    // Your custom options
+  });
 };
