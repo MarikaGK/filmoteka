fd323d2c5afc2a0ec69c3395c521976d4d1aa735
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-04-24 18:36:50 +0200

    add function renderCategoriesBox

---
+++ src/js/rendering/render-multiselectBox.js
@@ -0,0 +1,13 @@
+export const renderCategoriesBox = () => {
+    const categories = localStorage.getItem('movieGenresIdsArray');
+    const parsedCategories = JSON.parse(categories);
+    const multiselectBox = document.querySelector('.header-input__categoriesBox')
+    const markup = parsedCategories.map((category) => `<label class="header-input__label" for="${category.id}">${category.name}
+    <input type="checkbox" class="header-input__checkbox" id="${category.id}">
+    </label>`
+    )
+        .join('');
+    multiselectBox.insertAdjacentHTML('beforeend', markup)
+
+}
+
