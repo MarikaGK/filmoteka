e7ae74e7442e22b1e19f9537e0f8fa9b71442120
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-04-25 12:01:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,17 +18,17 @@ import { discoveryHandler } from './js/utils/categories-filter.js';
 
 const FORM_DOM = document.querySelector('.header-input__form');
 const GALLERY_DOM = document.querySelector('.gallery');
-const BROWSE_BUTTON = document.querySelector('.header-nav__browse')
-const FIND_BUTTON = document.querySelector('.header-nav__selectButton')
-const CATEGORIES_BOX = document.querySelector('.header-nav__categoriesBox')
+const BROWSE_BUTTON = document.querySelector('.header__browse');
+const FIND_BUTTON = document.querySelector('.header__selectButton');
+const CATEGORIES_BOX = document.querySelector('.header__categoriesBox');
 
 FORM_DOM.addEventListener('submit', handleSubmit);
 GALLERY_DOM.addEventListener('click', galleryHandler);
 FIND_BUTTON.addEventListener('click', discoveryHandler);
-BROWSE_BUTTON.addEventListener('click', (evt) => {
+BROWSE_BUTTON.addEventListener('click', evt => {
   evt.preventDefault();
-  CATEGORIES_BOX.classList.toggle('is-hidden')
-})
+  CATEGORIES_BOX.classList.toggle('is-hidden');
+});
 
 getMovieGenresAndSaveToStore();
 startLoader();
@@ -42,8 +42,6 @@ renderPagination();
 // paginationClicker.addEventListener("click", ()=>{
 //     console.log(paginationClicker.value)
 // })
-
-
 
 //* Scroll site to top
 const SCROLL_UP_BUTTON_DOM = document.querySelector('.scroll-up-arrow');
