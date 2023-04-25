cd912b32c3c009f0d2f4f940757bd09aa97cbf02
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-04-25 17:11:32 +0200

    Update render-pagination.js

--- src/js/rendering/render-pagination.js
+++ src/js/rendering/render-pagination.js
@@ -11,6 +11,11 @@ const perPage = 20
 const visiblePages = 5
 const prevPage = document.querySelector('.tui-prev')
 const nextPage = document.querySelector('.tui-next')
+const indexButton = document.querySelector('.header-nav__home-page-btn')
+indexButton.addEventListener('click', ()=>{
+const popularParameter = 1
+renderPagination()
+})
 
 const watchedArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50];
 const queueArray = [101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125];
