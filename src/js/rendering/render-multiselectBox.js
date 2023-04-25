da44204f7e9860dcee1a6d7d30b4e7da2e4ae3be
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-04-25 12:06:50 +0200

    browse bugfix

--- src/js/rendering/render-multiselectBox.js
+++ src/js/rendering/render-multiselectBox.js
@@ -1,9 +1,9 @@
 export const renderCategoriesBox = () => {
     const categories = localStorage.getItem('movieGenresIdsArray');
     const parsedCategories = JSON.parse(categories);
-    const multiselectBox = document.querySelector('.header-nav__categoriesBox')
-    const markup = parsedCategories.map((category) => `<label class="header-nav__label" for="${category.id}">${category.name}
-    <input type="checkbox" class="header-nav__checkbox" id="${category.id}">
+    const multiselectBox = document.querySelector('.header__categoriesBox')
+    const markup = parsedCategories.map((category) => `<label class="header__label" for="${category.id}">${category.name}
+    <input type="checkbox" class="header__checkbox" id="${category.id}">
     </label>`
     )
         .join('');
--- src/js/utils/categories-filter.js
+++ src/js/utils/categories-filter.js
@@ -1,14 +1,12 @@
-import { getMoviesWithFilters } from "../fetching/fetch-movies";
-const CATEGORIES_BOX = document.querySelector('.header-nav__categoriesBox')
+import { getMoviesWithFilters } from '../fetching/fetch-movies';
+const CATEGORIES_BOX = document.querySelector('.header__categoriesBox');
 export const categoriesFilter = () => {
-    const checked = document.querySelectorAll('input[type="checkbox"]:checked');
-    return ([...checked].map(c => Number(c.id))).join();
-}
+  const checked = document.querySelectorAll('input[type="checkbox"]:checked');
+  return [...checked].map(c => Number(c.id)).join();
+};
 
-
-
-export const discoveryHandler = (evt) => {
-    evt.preventDefault();
-    getMoviesWithFilters();
-    CATEGORIES_BOX.classList.toggle('is-hidden')
-}
+export const discoveryHandler = evt => {
+  evt.preventDefault();
+  getMoviesWithFilters();
+  CATEGORIES_BOX.classList.toggle('is-hidden');
+};
