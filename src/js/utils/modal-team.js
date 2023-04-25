8b0167fcec6cbac14b8ba8be57fe6af2af701802
AlinaBielska <alinkabelka5@gmail.com>
2023-04-25 11:40:55 +0200

    dodałam usuwanie eventListener po zamknięciu modala

--- src/js/utils/modal-team.js
+++ src/js/utils/modal-team.js
@@ -3,27 +3,32 @@ const linkModalTeam = document.querySelector('.footer__wrapper-link');
 const modalTeam = document.querySelector('.backdrop');
 const modalClose = document.querySelector('.team_close');
 
+const closeModalTeamByBackdrop = (e) => {
+  if (
+    e.target.classList.contains('backdrop')
+  ) {
+    modalTeam.classList.add('is-hidden');
+    modalTeam.removeEventListener('click', closeModalTeamByBackdrop);
+  }
+};
+
+const closeModalTeamByIcon = (e) => {
+  modalTeam.classList.add('is-hidden');
+  modalClose.removeEventListener('click', closeModalTeamByIcon);
+  }
+
+const keyPress = (e) => {
+  if (e.key === 'Escape') {
+    modalTeam.classList.add('is-hidden');
+    modalTeam.removeEventListener('keydown', keyPress);
+  }
+};
+
 linkModalTeam.addEventListener('click', (e) => {
   e.preventDefault();
   modalTeam.classList.remove('is-hidden');
 });
 
-modalTeam.addEventListener('click', (e) => {
-  if (
-    e.target.classList.contains('backdrop')
-  ) {
-    modalTeam.classList.add('is-hidden');
-  }
-});
-
-modalClose.addEventListener('click', (e) => {
-    modalTeam.classList.add('is-hidden');
-  })
-
-const keyPress = (e) => {
-  if (e.key === 'Escape') {
-    modalTeam.classList.add('is-hidden');
-  }
-};
-
 modalTeam.addEventListener('keydown', keyPress);
+modalTeam.addEventListener('click', closeModalTeamByBackdrop);
+modalClose.addEventListener('click', closeModalTeamByIcon);
