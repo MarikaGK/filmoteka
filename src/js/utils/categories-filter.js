6f32b125243116dbed3abf93c21ac128b97812a5
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-04-25 16:34:17 +0200

    genres bugfix

--- src/js/utils/categories-filter.js
+++ src/js/utils/categories-filter.js
@@ -1,8 +1,8 @@
 import { getMoviesWithFilters } from '../fetching/fetch-movies';
 const CATEGORIES_BOX = document.querySelector('.header__categoriesBox');
 export const categoriesFilter = () => {
-  const checked = document.querySelectorAll('input[type="checkbox"]:checked');
-  return [...checked].map(c => Number(c.id)).join(',');
+  const checked = document.querySelectorAll('.header__checkbox:checked');
+  return [...checked].map(c => Number(c.id)).join();
 };
 
 export const discoveryHandler = evt => {
