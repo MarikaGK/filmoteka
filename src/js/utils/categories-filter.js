54d0616c3b657179ce01bad286d3cbcbd674fce1
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-04-25 14:26:04 +0200

    checklist bugfix

--- src/js/utils/categories-filter.js
+++ src/js/utils/categories-filter.js
@@ -2,7 +2,7 @@ import { getMoviesWithFilters } from '../fetching/fetch-movies';
 const CATEGORIES_BOX = document.querySelector('.header__categoriesBox');
 export const categoriesFilter = () => {
   const checked = document.querySelectorAll('input[type="checkbox"]:checked');
-  return [...checked].map(c => Number(c.id)).join();
+  return [...checked].map(c => Number(c.id)).join(',');
 };
 
 export const discoveryHandler = evt => {
