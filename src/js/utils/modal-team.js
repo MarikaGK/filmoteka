5e9c36ded6dc9a579173e63fd15b00d84d100e79
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-04-25 22:52:05 +0200

    backdrop fix

--- src/js/utils/modal-team.js
+++ src/js/utils/modal-team.js
@@ -1,10 +1,12 @@
 const linkModalTeam = document.querySelector('.footer__wrapper-link');
-const modalTeam = document.querySelector('.backdrop');
+const modalTeamBackdrop = document.querySelector('.backdrop');
+const modalTeam = document.querySelector('.team');
 const modalClose = document.querySelector('.team_close');
 
 const closeModalTeamByBackdrop = e => {
   if (e.target.classList.contains('backdrop')) {
-    modalTeam.classList.toggle('is-hidden');
+    modalTeam.classList.toggle('none');
+    modalTeamBackdrop.classList.toggle('is-hidden');
     modalTeam.removeEventListener('keydown', keyPress);
     modalTeam.removeEventListener('click', closeModalTeamByBackdrop);
     modalClose.removeEventListener('click', closeModalTeamByIcon);
@@ -12,7 +14,8 @@ const closeModalTeamByBackdrop = e => {
 };
 
 const closeModalTeamByIcon = e => {
-  modalTeam.classList.toggle('is-hidden');
+  modalTeam.classList.toggle('none');
+  modalTeamBackdrop.classList.toggle('is-hidden');
   modalTeam.removeEventListener('keydown', keyPress);
   modalTeam.removeEventListener('click', closeModalTeamByBackdrop);
   modalClose.removeEventListener('click', closeModalTeamByIcon);
@@ -20,7 +23,8 @@ const closeModalTeamByIcon = e => {
 
 const keyPress = e => {
   if (e.key === 'Escape') {
-    modalTeam.classList.toggle('is-hidden');
+    modalTeam.classList.toggle('none');
+    modalTeamBackdrop.classList.toggle('is-hidden');
     modalClose.removeEventListener('click', closeModalTeamByIcon);
     modalTeam.removeEventListener('click', closeModalTeamByBackdrop);
     modalTeam.removeEventListener('keydown', keyPress);
@@ -29,12 +33,13 @@ const keyPress = e => {
 
 export const addELToTeamModal = () => {linkModalTeam.addEventListener('click', e => {
   e.preventDefault();
-onShowTeamModal();
+  onShowTeamModal();
 });
 }
 
 const onShowTeamModal = () => {
-  modalTeam.classList.toggle('is-hidden');
+  modalTeam.classList.toggle('none');
+  modalTeamBackdrop.classList.toggle('is-hidden');
   modalTeam.addEventListener('keydown', keyPress);
   modalTeam.addEventListener('click', closeModalTeamByBackdrop);
   modalClose.addEventListener('click', closeModalTeamByIcon);
