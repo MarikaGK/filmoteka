100159ff866cd595724cabf06366e87244cd9e08
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-10-09 00:05:41 +0200

    Update firebase.js

--- src/js/firebase/firebase.js
+++ src/js/firebase/firebase.js
@@ -105,7 +105,7 @@ signOutBtn.addEventListener('click', () => {
       localStorage.remove('queue');
       navFirst.classList.toggle('header__none');
       navSecond.classList.toggle('header__none');
-      window.location.href = 'https://marikagk.github.io/filmoteka-js-team-project/index';
+      window.location.href = 'https://marikagk.github.io/filmoteka-js-team-project/';
     })
     .catch(error => {
       console.log('Error Sign Out');
