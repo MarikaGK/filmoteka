bd490213529b3e7f3e8d04ff38b70aef4596ed44
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2024-11-17 22:12:35 +0100

    fix loading movies by array of ids

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import {
   getPopularMovies,
   getMovieGenresAndSaveToStore,
-  getMovieById,
+  // getMovieById,
+  getMovieDetailsWithVideosById,
 } from './js/fetching/fetch-movies.js';
 import { handleSubmit } from './js/utils/search-form-handler.js';
 //toggle modal fn
 import { onShowModal, toggleModal } from './js/utils/modal-movie-details.js';
 import { setDarkOrNormalModeOnPageLoadFromLocalStorageState } from './js/utils/dark-mode-switch.js';
 import { showButtonOnScroll, scrollToTop } from './js/utils/scroll-to-top.js';
-import { renderPagination, saveFactorToLocalStorage, setPopularParameterToStorage } from './js/rendering/render-pagination.js';
+import {
+  renderPagination,
+  saveFactorToLocalStorage,
+  setPopularParameterToStorage,
+} from './js/rendering/render-pagination.js';
 import { addELToTeamModal } from './js/utils/modal-team.js';
 import { actualLibraryUpdateToStore } from './js/utils/store.js';
 import { startLoader } from './js/utils/loader.js';
@@ -18,8 +23,8 @@ const GALLERY_DOM = document.querySelector('.gallery');
 
 FORM_DOM.addEventListener('submit', handleSubmit);
 
-setPopularParameterToStorage(1)
-saveFactorToLocalStorage(2)
+setPopularParameterToStorage(1);
+saveFactorToLocalStorage(2);
 actualLibraryUpdateToStore('home');
 //* DARK MODE
 setDarkOrNormalModeOnPageLoadFromLocalStorageState();
@@ -32,7 +37,7 @@ GALLERY_DOM.addEventListener('click', evt => {
   const singleMovieCard = evt.target.parentElement.parentElement;
   if (!singleMovieCard.classList.contains('movie-card')) return;
   // getTrailerUrlByMovieId(singleMovieCard.dataset.movieId);
-  getMovieById(singleMovieCard.dataset.movieId);
+  getMovieDetailsWithVideosById(singleMovieCard.dataset.movieId);
   onShowModal(singleMovieCard.dataset.movieId);
   //zmiana klasy modala
   toggleModal();
